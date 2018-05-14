ec42a07185568e4f6c6fb420e6ab26b097ca95df
Nicholas Powers <njpowers7915@gmail.com>
2018-05-14 02:53:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
   end
   
   def invalid_triangle?
-    @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || (@side_one + @side_two < @side_three) || (@side_one + @side_three < @side_two) || (@side_two + @side_three < @side_one)
+    @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || ((@side_one + @side_two) < @side_three) || ((@side_one + @side_three) < @side_two) || ((@side_two + @side_three) < @side_one)
       begin
         raise TriangleError
       rescue TriangleError => error
