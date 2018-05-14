2b8e6d30eefdc3e0fa8bd664f26efe8210f6c66a
Nicholas Powers <njpowers7915@gmail.com>
2018-05-14 02:56:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,6 @@ class Triangle
   def invalid_triangle?
     @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || ((@side_one + @side_two) < @side_three) || ((@side_one + @side_three) < @side_two) || ((@side_two + @side_three) < @side_one)
       begin
-      binding.pry
         raise TriangleError
       rescue TriangleError => error
           puts error.message
